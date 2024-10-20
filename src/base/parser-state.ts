650d59ec985eb4f9ce08be70a1a8b028cdbffa27
sharadtomer <sharadtomer@outlook.com>
2024-10-21 03:04:22 +0530

    base parser class

---
+++ src/base/parser-state.ts
@@ -0,0 +1,70 @@
+/**
+ * Paser state structure
+ */
+export interface IParserState {
+  /** Initial input string */
+  inputString: string;
+  /** Current input string  */
+  index: number;
+  /** Result of running parser */
+  result: any;
+  /** If parsing resulted in any error */
+  isError: boolean;
+  /** Error resulting from parsing */
+  err: any;
+}
+
+/**
+ * Utility class with a bunch of methods to handle state more easily
+ */
+export class StateUtils {
+
+  /**
+  * Get initial parser state for the provided input string
+  * @param input initial input string
+  */
+  static newState(input: string): IParserState {
+    return {
+      inputString: input,
+      index: 0,
+      result: null,
+      isError: false,
+      err: null
+    };
+  }
+
+  /**
+   * return state with provided result immutably
+   * 
+   */
+  static withResult(state: IParserState, result: any): IParserState {
+    return {
+      ...state,
+      result
+    };
+  }
+
+  /**
+   * return state with provided error immutably
+   * 
+   */
+  static withError(state: IParserState, err: any): IParserState {
+    return {
+      ...state,
+      err
+    };
+  }
+
+  /**
+   * update state result immutably
+   * 
+   */
+  static updateResult(state: IParserState, result: any, newIndex: number): IParserState {
+    return {
+      ...state,
+      index: newIndex,
+      result
+    };
+  }
+
+}
--- src/base/parser.ts
+++ src/base/parser.ts
@@ -1,3 +1,114 @@
+import { IParserState, StateUtils } from "parser-state";
+
+/**
+ * Parser function that transforms parser state to some other state
+ */
+export type TStateTransformFunction = (state: IParserState) => IParserState;
+
+/**
+ * Transform function that transform result from one form to another
+ * @param result previous result
+ */
+export type TResultTransformFunction = (result: any) => any;
+
+/**
+ * Transform function that transform error from one form to another
+ * @param state Parser state that has error
+ * @returns transformed error
+ */
+export type TErrorTransformFunction = (state: IParserState) => any;
+
+/**
+ * Parser selector function that returns next parser of use based on the presious parser result
+ */
+export type TParserSelectorFunction = (result: any) => Parser;
+
+/**
+ * Main parser definition
+ * parser constructor takes in StateTransformFunction and parsers the input string accordingly
+ * 
+ */
 export class Parser {
+
+  /** StateTransformFunction of this parser */
+  parseFn: TStateTransformFunction;
+
+  constructor(fn: TStateTransformFunction) {
+    this.parseFn = fn;
+  }
+
+  /**
+   * Parse the provide input string and report any result or error in the
+   * form of IParserState
+   * @param input string to parse
+   */
+  run(input: string): IParserState {
+    const state = StateUtils.newState(input);
+    return this.parseFn(state);
+  }
+
+  /**
+   * Transform the result of parser into another state, 
+   * do nothing if parsing result in error
+   * @param resultMapfn ResultTransformFunction to use for result transformation
+   * @returns New parser that returns the transformed result after parsing using the original parser
+   */
+  map(resultMapfn: TResultTransformFunction): Parser {
+    return new Parser((state) => {
+      // parse with current transformation function
+      const nextState = this.parseFn(state);
+
+      // do nothing for error state
+      if (nextState.isError) {
+        return nextState;
+      }
+
+      const transformedResult = resultMapfn(nextState.result);
+      return StateUtils.withResult(nextState, transformedResult);
+    });
+  }
+
+  /**
+   * Transform the error of parser into another state, 
+   * do nothing if parsing completes without any error
+   * @param errMapfn ErrorTransformFunction to use for error transformation
+   * @returns New parser that returns the transformed error after parsing using the original parser
+   */
+  errMap(errMapfn: TErrorTransformFunction): Parser {
+    return new Parser((state) => {
+      // parse with current transformation function
+      const nextState = this.parseFn(state);
+
+      // do nothing for successful parsing
+      if (!nextState.isError) {
+        return nextState;
+      }
+
+      const transformedError = errMapfn(nextState);
+      return StateUtils.withError(nextState, transformedError);
+    });
+  }
+
+  /**
+   * select the next parser dynamically based on result of current parser
+   * 
+   * @param selectorFn selector function that the next parser to use based on result of current parser
+   * @returns New parser that runs the current parser and next dynamically selcted parser in sequence
+   */
+  chain(selectorFn: TParserSelectorFunction): Parser {
+    return new Parser((state) => {
+      // parse with current transformation function
+      const nextState = this.parseFn(state);
+
+      // do nothing for error state
+      if (nextState.isError) {
+        return nextState;
+      }
+
+      // get next parser and parse
+      const nextParser = selectorFn(nextState.result);
+      return nextParser.parseFn(nextState);
+    });
+  }
 
 }
