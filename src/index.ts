33e69f17e7d75c65e4d087dc696d359df3dcdca5
sharadtomer <sharadtomer@outlook.com>
2025-02-27 01:38:46 +0530

    export more things from base

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * as StringParsers from './parsers/strings';
 export * as NumberParsers from './parsers/numbers';
+export * from './base/parser';
+export * from './base/parser-state';
+export * from './base/combinators';
